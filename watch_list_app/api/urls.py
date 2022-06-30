7fd6f0837ea267ff7ed5a83f0c816fbd3894ed07
Kasir <kasir.barati@gmail.com>
2022-06-30 21:31:53 +0430

    add: upload file endpoint

--- watch_list_app/api/urls.py
+++ watch_list_app/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import movie_list
 from .views import movie_details
+from .views import upload_movie
 
 urlpatterns = [
     path('', movie_list),
-    path('<int:id>', movie_details)
+    path('<int:id>', movie_details),
+    path('upload', upload_movie)
 ]
--- watch_list_app/api/views.py
+++ watch_list_app/api/views.py
@@ -1,8 +1,21 @@
+from rest_framework.exceptions import bad_request
+from zoneinfo import ZoneInfo
+from rest_framework import status
+from django.core.files import File
+import datetime
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from ..models import Movie
 from .serializers import MovieSerializer
+from .serializers import UploadMovieSerializer
+"""
+IDK but my gut tells me how Python work is at odds with its documentation. 
+This is just my Opinion.
+Here it says use relative imports: https://peps.python.org/pep-0328/
+But here it defines another rule: https://docs.python.org/3.9/tutorial/modules.html#intra-package-references
+"""
+from shared.save_uploaded_file import save_uploaded_file
 
 
 """
@@ -24,4 +37,29 @@ def movie_details(req: Request, id: int) -> Response:
     serialized_movie = MovieSerializer(movie)
 
     return Response(serialized_movie.data)
+
+
+@api_view(["POST"])
+def upload_movie(req: Request) -> Response:
+    if 'file' not in req.FILES.file:
+        return bad_request(req, 'Please select a file')
+
+    serialized_movie = UploadMovieSerializer(data=req.data)
+    sent_file: File = req.FILES.file
+    save_uploaded_file(sent_file)
+
+    # Timezone is really a hard thing to deal. So I decided to keep it in zero timezone
+    now = datetime.datetime.now(tz=ZoneInfo("Etc/GMT"))
+    created_move = Movie.objects.create(
+        name=sent_file.name,
+        description=f"File uploaded at {now}",
+        active=True
+    )
+    created_move.save()
+
+    serialized_movie = MovieSerializer(created_move)
+    return Response(
+        serialized_movie, 
+        status=status.HTTP_201_CREATED
+    )
 
